786713c4d803411faad229cf0eeafa7332c184bd
zzayphod <whcarter3@gmail.com>
2020-07-10 04:03:44 -0500

    blackout mode added, favicon added, optimize for mobile

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var spaces = [
 "Incest is mentioned",
 "Zzay is late",
 "ZZAYOOM",
-"Feather cyberbullies Zzay",
+"Feather bullies Zzay",
 "Scarebits used",
 "Donation occurs",
 "Zzay gets raided",
@@ -24,13 +24,13 @@ var spaces = [
 "Follow Goal Met",
 "Sub Goal Met",
 "Accidental Mute",
-"Partner in Chat",
+"Partner appears in Chat",
 "Quote is added",
 "Gifted subs",
 "Zzay or Bae dies from fall damage",
 "Light color change",
 "Baephod goes to sleep",
-"Zzay makes a pass at light",
+"Zzay makes a pass at Light",
 "Baephod beats Zzayphod at any game",
 "Baephod beats a Mario Maker Level",
 "Gravy needs attention",
@@ -42,49 +42,73 @@ var spaces = [
 "Kenzie makes an appearance",
 "Bae gets a snacc",
 "Zzay eats Sour Skittles",
-"Zzay mentions his OnlyFans"
+"Zzay mentions his OnlyFans",
+"Hype Train Activated",
+"Hype Train Level 2",
+"Hype Train Level 3",
+"Hype Train Level 4",
+"Hype Train Level 5",
+"Buttsbot targets Meeg"
 ];
 
-var winner = false;
+var gameOver = false;
+var blackout = false;
 
 
 $('td').not("#freeSpace").click(function(){
-  if(winner === true){
+  if(gameOver === true){
     return;
   }
   $(this).toggleClass('selected');
-  if( 
-    //horizontal win
-    ($('td').eq(0).hasClass('selected') && $('td').eq(1).hasClass('selected') && $('td').eq(2).hasClass('selected') && $('td').eq(3).hasClass('selected') && $('td').eq(4).hasClass('selected')) ||
-    ($('td').eq(5).hasClass('selected') && $('td').eq(6).hasClass('selected') && $('td').eq(7).hasClass('selected') && $('td').eq(8).hasClass('selected') && $('td').eq(9).hasClass('selected')) ||
-    ($('td').eq(10).hasClass('selected') && $('td').eq(11).hasClass('selected') && $('td').eq(12).hasClass('selected') && $('td').eq(13).hasClass('selected') && $('td').eq(14).hasClass('selected')) ||
-    ($('td').eq(15).hasClass('selected') && $('td').eq(16).hasClass('selected') && $('td').eq(17).hasClass('selected') && $('td').eq(18).hasClass('selected') && $('td').eq(19).hasClass('selected')) ||
-    ($('td').eq(20).hasClass('selected') && $('td').eq(21).hasClass('selected') && $('td').eq(22).hasClass('selected') && $('td').eq(23).hasClass('selected') && $('td').eq(24).hasClass('selected')) ||
-    //vertical win
-    ($('td').eq(0).hasClass('selected') && $('td').eq(5).hasClass('selected') && $('td').eq(10).hasClass('selected') && $('td').eq(15).hasClass('selected') && $('td').eq(20).hasClass('selected')) ||
-    ($('td').eq(1).hasClass('selected') && $('td').eq(6).hasClass('selected') && $('td').eq(11).hasClass('selected') && $('td').eq(16).hasClass('selected') && $('td').eq(21).hasClass('selected')) ||
-    ($('td').eq(2).hasClass('selected') && $('td').eq(7).hasClass('selected') && $('td').eq(12).hasClass('selected') && $('td').eq(17).hasClass('selected') && $('td').eq(22).hasClass('selected')) ||
-    ($('td').eq(3).hasClass('selected') && $('td').eq(8).hasClass('selected') && $('td').eq(13).hasClass('selected') && $('td').eq(18).hasClass('selected') && $('td').eq(23).hasClass('selected')) ||
-    ($('td').eq(4).hasClass('selected') && $('td').eq(9).hasClass('selected') && $('td').eq(14).hasClass('selected') && $('td').eq(19).hasClass('selected') && $('td').eq(24).hasClass('selected')) ||
-    //horizontal win
-    ($('td').eq(0).hasClass('selected') && $('td').eq(6).hasClass('selected') && $('td').eq(12).hasClass('selected') && $('td').eq(18).hasClass('selected') && $('td').eq(24).hasClass('selected')) ||
-    ($('td').eq(4).hasClass('selected') && $('td').eq(8).hasClass('selected') && $('td').eq(12).hasClass('selected') && $('td').eq(16).hasClass('selected') && $('td').eq(20).hasClass('selected'))
+  if(blackout === false){
+    if(
+      //horizontal win
+      ($('td').eq(0).hasClass('selected') && $('td').eq(1).hasClass('selected') && $('td').eq(2).hasClass('selected') && $('td').eq(3).hasClass('selected') && $('td').eq(4).hasClass('selected')) ||
+      ($('td').eq(5).hasClass('selected') && $('td').eq(6).hasClass('selected') && $('td').eq(7).hasClass('selected') && $('td').eq(8).hasClass('selected') && $('td').eq(9).hasClass('selected')) ||
+      ($('td').eq(10).hasClass('selected') && $('td').eq(11).hasClass('selected') && $('td').eq(12).hasClass('selected') && $('td').eq(13).hasClass('selected') && $('td').eq(14).hasClass('selected')) ||
+      ($('td').eq(15).hasClass('selected') && $('td').eq(16).hasClass('selected') && $('td').eq(17).hasClass('selected') && $('td').eq(18).hasClass('selected') && $('td').eq(19).hasClass('selected')) ||
+      ($('td').eq(20).hasClass('selected') && $('td').eq(21).hasClass('selected') && $('td').eq(22).hasClass('selected') && $('td').eq(23).hasClass('selected') && $('td').eq(24).hasClass('selected')) ||
+      //vertical win
+      ($('td').eq(0).hasClass('selected') && $('td').eq(5).hasClass('selected') && $('td').eq(10).hasClass('selected') && $('td').eq(15).hasClass('selected') && $('td').eq(20).hasClass('selected')) ||
+      ($('td').eq(1).hasClass('selected') && $('td').eq(6).hasClass('selected') && $('td').eq(11).hasClass('selected') && $('td').eq(16).hasClass('selected') && $('td').eq(21).hasClass('selected')) ||
+      ($('td').eq(2).hasClass('selected') && $('td').eq(7).hasClass('selected') && $('td').eq(12).hasClass('selected') && $('td').eq(17).hasClass('selected') && $('td').eq(22).hasClass('selected')) ||
+      ($('td').eq(3).hasClass('selected') && $('td').eq(8).hasClass('selected') && $('td').eq(13).hasClass('selected') && $('td').eq(18).hasClass('selected') && $('td').eq(23).hasClass('selected')) ||
+      ($('td').eq(4).hasClass('selected') && $('td').eq(9).hasClass('selected') && $('td').eq(14).hasClass('selected') && $('td').eq(19).hasClass('selected') && $('td').eq(24).hasClass('selected')) ||
+      //horizontal win
+      ($('td').eq(0).hasClass('selected') && $('td').eq(6).hasClass('selected') && $('td').eq(12).hasClass('selected') && $('td').eq(18).hasClass('selected') && $('td').eq(24).hasClass('selected')) ||
+      ($('td').eq(4).hasClass('selected') && $('td').eq(8).hasClass('selected') && $('td').eq(12).hasClass('selected') && $('td').eq(16).hasClass('selected') && $('td').eq(20).hasClass('selected'))
 
-  ){
-    alert("Bingo Mother fucker");
-    winner = true;
+    ){
+      $('#winModal').modal('show');
+    }
+  } else {
+    if($('td.selected').length === $('td').length){
+      $('#ultimateWinModal').modal('show');
+    }
   }
 })
 
 $('#clear').click(function(){
   $('td').not("#freeSpace").removeClass('selected');
-  winner = false;
+  gameOver = false;
 })
 
-$('#newCard').click(function(){
+$('.newCard').click(function(){
   $('td').not("#freeSpace").removeClass('selected');
   randomizeSquares();
-  winner = false;
+  gameOver = false;
+  blackout = false;
+  $('#gameMode').text("5 in a row");
+})
+
+$('.gameOver').click(function(){
+  gameOver = true;
+})
+
+$('#blackoutButton').click(function (){
+  blackout = true;
+  $('#winModal').modal('toggle');
+  $('#gameMode').text("Blackout");
 })
 
 function randomizeSquares (){
